013c10f09640e18d8468fd755855649f5da58008
Raina <han.raina999@gmail.com>
2022-07-21 11:22:48 -0700

    Update name var

--- eduhabits/ViewController.swift
+++ eduhabits/ViewController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-   
+        nameLabel.isHidden = true
+        helloLabel.isHidden = true
+    }
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var askNameLabel: UILabel!
     
+    @IBOutlet weak var nameField: UITextField!
+
+    @IBAction func nameField(_ sender: Any) {
+        if (helloLabel.isHidden == false) {
+            helloLabel.isHidden = true
+            } else {
+                helloLabel.isHidden = false
+                
+        if (nameLabel.isHidden == false) {
+              nameLabel.isHidden = true
+            } else {
+                nameLabel.isHidden = false
+                
+        let mText = nameField.text
+        nameLabel.text = mText
+
+                if (askNameLabel.isHidden == false) {
+                    askNameLabel.isHidden = true
+                        }
+    }
+    
+}
+    }
+    
+}
