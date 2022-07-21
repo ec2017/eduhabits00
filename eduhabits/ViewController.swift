10695561949e0bcdce9aada861b3ea2a07df1d4e
Raina <han.raina999@gmail.com>
2022-07-21 11:48:26 -0700

    New update

--- eduhabits/ViewController.swift
+++ eduhabits/ViewController.swift
@@ -10,36 +10,25 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameLabel.isHidden = true
-        helloLabel.isHidden = true
+
     }
     
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var helloLabel: UILabel!
-    @IBOutlet weak var askNameLabel: UILabel!
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let secondViewController = segue.destination as! secondViewController
+        secondViewController.userName = nameField.text!
+    }
     
     @IBOutlet weak var nameField: UITextField!
 
-    @IBAction func nameField(_ sender: Any) {
-        if (helloLabel.isHidden == false) {
-            helloLabel.isHidden = true
-            } else {
-                helloLabel.isHidden = false
-                
-        if (nameLabel.isHidden == false) {
-              nameLabel.isHidden = true
-            } else {
-                nameLabel.isHidden = false
-                
-        let mText = nameField.text
-        nameLabel.text = mText
+    @IBAction func continueButton(_ sender: Any) {
+        if nameField.text != "" {
+            performSegue(withIdentifier: "segue1", sender: self)
+        
+    }
+    
+    }
+}
 
-                if (askNameLabel.isHidden == false) {
-                    askNameLabel.isHidden = true
-                        }
-    }
+
     
-}
-    }
-    
-}
+
---
+++ eduhabits/secondViewController.swift
@@ -0,0 +1,34 @@
+//
+//  secondViewController.swift
+//  eduhabits
+//
+//  Created by scholar on 7/21/22.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    var userName = String()
+    
+
+    @IBOutlet weak var welcomeMessage: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        welcomeMessage.text = "Welcome \(userName) to Eduhabits!"
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    }
+}
